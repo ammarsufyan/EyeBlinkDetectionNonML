342dc2c949719e7ce8e1fe5c3a27659c3e453808
Ammar Sufyan <63483228+ammarsufyan@users.noreply.github.com>
2025-06-11 15:41:22 +0700

    Initial Commit

---
+++ EyeBlinkDetectionNonML/EyeBlinkDetectionNonMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeBlinkDetectionNonMLApp.swift
+//  EyeBlinkDetectionNonML
+//
+//  Created by Ammar Sufyan on 11/06/25.
+//
+
+import SwiftUI
+
+@main
+struct EyeBlinkDetectionNonMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
